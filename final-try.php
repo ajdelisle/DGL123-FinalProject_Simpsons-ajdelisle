86ea50d814bc47fc567875e24bb3a00c5bee959e
Amanda Delisle <71227090+ajdelisle@users.noreply.github.com>
2021-12-08 21:07:27 -0500

    Display array from database
    *not formatted properly yet

--- final-try.php
+++ final-try.php
@@ -9,17 +9,32 @@ $conn = mysqli_connect('localhost', 'root', '', 'Simpsons');
 
 //select data to show
 $query = 'SELECT * FROM addbyhand';
+?>
+<ul class="characters_items">
+    <li class="characters_itemContainer">
 
-if ($results = mysqli_query($conn, $query)){
-    while ($row = mysqli_fetch_array($results)){
-        print "<p><h3>{$row['first_name']} {$row['last_name']}</h3>
-        {$row['age']}<br>
-        {$row['occupation']}<br>
-        {$row['voiced_by']}<br>
-        
-        </p><hr />\n";
-    }
-
-
-}
-
+    <div class="character_item">
+        <img src="images/homer.png" alt="homer" class="character_image">
+    
+    <?php if ($results = mysqli_query($conn, $query)):?>
+        <?php while ($row = mysqli_fetch_array($results)):?>
+            
+            <div class="character_info">
+                <h3 class="character_name"><?= $row['first_name'] . $row['last_name']?></h3>
+            
+            <div class="characters_age characters_attribute">
+                <b>Age:</b><?=$row['age']?>
+            </div>
+            <div class="characters_occupation characters_attribute">
+                <b>Occupation:</b><?=$row['occupation']?>
+            </div>
+            <div class="characters_voicedBy characters_attribute">
+                <b>Voiced by:</b><?=$row['voiced_by'] ?>
+            </div>
+            
+            </div> 
+        <?php endwhile ?>
+    <?php endif ?>
+</div>
+</li>
+</ul>
